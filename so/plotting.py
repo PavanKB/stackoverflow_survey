dae74ff4f20e0442fd2d3cf4a204208f6b33df8c
PavanKB <pavan.infinium@gmail.com>
2019-11-11 12:46:31 +0800

    EDA code cleanup
    updated plotting functions to sort/order the count plots
    Update the python notebook to use the refreshed function

--- so/plotting.py
+++ so/plotting.py
@@ -25,12 +25,17 @@ def plot_data_dist_grid(data, col_idx, n_col=4, n_row=4, order=None):
         _ = g.set_xticklabels(rotation=30, labels=g.get_xticklabels())
 
 
-def plot_col_dist(df, col_idx, order=None, rotation=90, fig_size=(10, 5)):
+def plot_col_dist(df, col_idx, order=None, sort=True, rotation=90, fig_size=(10, 5)):
     # Create the subplot to host the plots.
     fig, ax = plt.subplots(figsize=fig_size)
 
     if order:
         g = sns.countplot(data=df, x=col_idx, order=order, ax=ax)
     else:
-        g = sns.countplot(data=df, x=col_idx, ax=ax)
+        if sort:
+            order = df[col_idx].value_counts().index
+            g = sns.countplot(data=df, x=col_idx, order=order, ax=ax)
+        else:
+            g = sns.countplot(data=df, x=col_idx, ax=ax)
+
     _ = g.set_xticklabels(rotation=rotation, labels=g.get_xticklabels())
