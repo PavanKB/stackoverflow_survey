32cd9948b1b6c40d386b56126bd39ff3d52bd79b
PavanKB <pavan.infinium@gmail.com>
2019-11-18 00:21:41 +0800

    Update setup.py

--- so/setup.py
+++ so/setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
-
-setuptools.setup(
-     name='kaggle_stackoverflow_survey',
+setup(
+     name='stackoverflow_survey',
      version='0.0.1',
      scripts=[''],
      author="Pavan Burra",
      author_email="pavank.burra@gmail.com",
-     description="Contains function to analyse the SO survey data from Kaggle",
-     long_description=long_description,
+     description="Contains function to analyse the SO survey data.",
+     long_description="",
      long_description_content_type="text/markdown",
      url="",
      packages=find_packages(),
